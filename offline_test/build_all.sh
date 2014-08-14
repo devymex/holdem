dd09841133082b30347db355c21038d2d1b15564
Zhuoyue Zhao <zzy7896321@163.com>
2014-08-15 00:40:15 +0800

    offline test: build_all.sh

---
+++ offline_test/build_all.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#set -x
+
+rm -rf tmp
+rm -rf bin
+mkdir -p bin
+
+client_path="../client"
+
+if [[ ${OSTYPE} == "linux-gnu" || ${OSTYPE} == "darwin"*  ]]; then
+	MAKE=make
+	MAKEFILE=Makefile
+	EXEC_SUFFIX=""
+elif [[ ${OSTYPE} == "msys" ]]; then
+	MAKE=mingw32-make
+	MAKEFILE=Makefile-mingw
+	EXEC_SUFFIX=.exe
+elif [[ ${OSTYPE} == "cygwin" ]]; then
+	echo "CANNOT run in cygwin"
+	exit 1
+else
+	echo "UNKNOWN OS"
+	exit 1
+fi
+
+for zip_file in `ls *.zip`
+do
+	name=`echo ${zip_file} | sed 's/\.zip//'`
+	echo "======================================="
+	unzip -o ${name}.zip
+
+	if [[ ! -e ${name} ]]; then
+		echo "[ERROR] file not found"
+		rm -rf ${name}
+		continue
+	fi
+
+	if [[ `ls ${name}/*.cpp 2>/dev/null` ]]; then
+		echo "cpp found, compiling"
+		mkdir tmp
+		cp -f ${client_path}/* tmp/
+		cp -f ${name}/*.cpp tmp/
+		cp -f ${name}/*.h tmp/
+
+		AI=`sed -n '/AI = / s/AI = //p' ${name}/Makefile`
+		if [[ ! ${AI} ]]; then
+			echo "AI is not properly set"
+		else
+			cd tmp
+			sed 's/AI = example/AI = '${AI}'/' ${MAKEFILE} > Makefile
+			${MAKE}
+
+			if [[ $? -eq 0 ]]; then
+				cp -f ./client${EXEC_SUFFIX} ../bin/${name}${EXEC_SUFFIX}
+			else
+				echo "compile error"
+			fi
+			cd ..
+			rm -rf tmp
+		fi
+	elif [[ `ls ${name}/*.py 2>/dev/null` ]]; then
+		echo "py found, copying"
+		cp -f `ls ${name}/*.py` bin/${name}.py
+	else
+		echo "no implementation found"
+	fi
+
+	rm -rf ${name}
+done
